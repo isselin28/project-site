c50304e268512fc1b6456d124b27ab583f34b93a
isselin28 <isselin.moektijono@gojek.com>
2022-07-19 02:03:26 +0800

    [Profile] add profile photo and icons

--- src/Blog.js
+++ src/Blog.js
@@ -3,13 +3,11 @@ import Profile from "./components/Profile/Profile";
 import TechList from "./components/TechList/TechList";
 import MenuBar from "./components/MenuBar/MenuBar";
 import "./Blog.css";
-import { Link } from "react-router-dom";
 
 function Blog() {
   return (
     <div class="pageContainer">
       <div class="leftContainer">
-        <Link to="/">back to home</Link>
         <div class="profileContainer">
           <Profile />
         </div>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,25 @@
 import React from "react";
 import styles from "./Profile.module.css";
+import photo from "../../assets/profile.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+
 function Profile() {
   return (
-    <div>
-      <div class={styles.title}>
-        Work &amp; Personal
-        <br />
-        Projects
+    <div class={styles.container}>
+      <img src={photo} alt="profile" class={styles.photo} />
+      <div class={styles.iconWrapper}>
+        <Link to="/" class={styles.icon}>
+          <FontAwesomeIcon icon={faHouse} size="lg" />
+        </Link>
+        <a
+          href="https://www.linkedin.com/in/isselinmoektijono/"
+          class={styles.icon}
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="xl" />
+        </a>
       </div>
     </div>
   );
