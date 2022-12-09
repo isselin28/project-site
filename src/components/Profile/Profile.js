c8a88a453594bbb79276c5db583692d4d4ee33ae
isselin28 <isselinm28@gmail.com>
2022-12-10 00:28:27 +0800

    [Profile] add email icon

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import styles from "./Profile.module.css";
 import photo from "../../assets/profile.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 
 function Profile() {
   return (
@@ -16,6 +17,13 @@ function Profile() {
           rel="noreferrer"
         >
           <FontAwesomeIcon icon={faLinkedin} size="xl" />
+        </a>
+        <a
+          href="mailto:isselinm28@gmail.com"
+          class={styles.icon}
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faEnvelopeSquare} size="xl" />
         </a>
       </div>
     </div>
