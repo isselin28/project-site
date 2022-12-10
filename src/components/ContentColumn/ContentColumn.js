71ffbe96ef3e3b6306e1466272650205d7c137c1
isselin28 <isselinm28@gmail.com>
2022-12-10 23:16:34 +0800

    [Content] Change user intro

--- src/components/ContentColumn/ContentColumn.js
+++ src/components/ContentColumn/ContentColumn.js
@@ -6,10 +6,22 @@ import personal from "../../assets/personal.json";
 import activities from "../../assets/activities";
 import arts from "../../assets/arts";
 import { isMobileDevice } from "../../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faHouse } from "@fortawesome/free-solid-svg-icons";
+
 function ContentColumn() {
   const isMobile = isMobileDevice();
   return (
     <div className={isMobile ? styles.mobileContainer : styles.container}>
+      <div className={styles.name}>Isselin Moektijono</div>
+      <div className={styles.intro}>
+        {/* <FontAwesomeIcon icon={faHouse} className={styles.icon} />
+        Developer, Designer, Loves to imagine a different world. &nbsp;
+        <FontAwesomeIcon icon={faLocationDot} className={styles.icon} />
+        Singapore */}
+      </div>
+      <hr />
+      <br />
       <Article text={gojek} />
       <Article text={personal} />
       <Article text={activities} />
